746fb450a459753f724bdb735b78a985ffddd75a
Ingrid Beatrice de Oliveira Santos <ingridbosantos@gmail.com>
2026-09-24 10:21:00 -0300

    commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -7,14 +7,14 @@ import Ionicons, { Ioicon } from '@expo/vector-icons/Ionicons';
 export default function App() {
   return (
     <GluestackUIProvider mode="dark">
-      <Tabs initialRouteName="Pagina1" screenOptions={{headerShown: false }}>
-        <Tabs.Screen name='Pagina1' options={{title: "home", tabBarIcon:({color, size})=>(
+      <Tabs initialRouteName="pagina1" screenOptions={{headerShown: false }}>
+        <Tabs.Screen name='pagina1' options={{title: "home", tabBarIcon:({color, size})=>(
           <Ionicons name="home-outline" size={size} color={color}/>
         )}} />
-         <Tabs.Screen name='Pagina2'options={{title: "Favoritos", tabBarIcon:({color, size})=>(
+         <Tabs.Screen name='pagina2'options={{title: "Favoritos", tabBarIcon:({color, size})=>(
           <Ionicons name="heart-outline" size={size} color={color}/>
         )}} />
-          <Tabs.Screen name='Pagina3'options={{title: "Perfil", tabBarIcon:({color, size})=>(
+          <Tabs.Screen name='pagina3'options={{title: "Perfil", tabBarIcon:({color, size})=>(
           <Ionicons name="person-outline" size={size} color={color}/>
         )}} />
           <Tabs.Screen name='index' options={{ href: null }} />
